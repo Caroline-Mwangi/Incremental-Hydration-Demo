1ad5406995d84c51b997daf5d18c1519d5b5334b
Caroline Mwangi <cwmwangi50@gmail.com>
2026-08-24 18:47:22 +0300

    feat: setup incremental hydration provider

--- Whack-A-Bug/src/app/app.ts
+++ Whack-A-Bug/src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SiteHeader } from './components/site-header/site-header';
+import { MissionBriefing } from './components/mission-briefing/mission-briefing';
+import { ScrollGap } from './components/scroll-gap/scroll-gap';
+import { ArcadeConsole } from './components/arcade-console/arcade-console';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SiteHeader, MissionBriefing, ScrollGap, ArcadeConsole],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
